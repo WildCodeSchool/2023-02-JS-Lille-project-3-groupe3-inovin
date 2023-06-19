f87f75e3f1575aabeb76d157f1e0017202f20253
MarieVanGreves <keiiskyx@gmail.com>
2023-06-19 10:31:28 +0200

    First Commit

--- frontend/src/pages/AtelierCreation/AtelierCreation.jsx
+++ frontend/src/pages/AtelierCreation/AtelierCreation.jsx
@@ -1,5 +1,13 @@
+import Bouteille from "../../components/Bouteille/Bouteille";
+import Apercu from "../../components/Apercu/Apercu";
+
 function AtelierCreation() {
-  return <div />;
+  return (
+    <div>
+      <Apercu />
+      <Bouteille />
+    </div>
+  );
 }
 
 export default AtelierCreation;
