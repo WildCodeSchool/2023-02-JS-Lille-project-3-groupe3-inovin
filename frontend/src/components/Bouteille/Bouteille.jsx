a4c95097d882b78af7d765fb69a289a94ad89a7a
MarieVanGreves <keiiskyx@gmail.com>
2023-06-20 09:36:04 +0200

    Some ajustments in CSS.

--- frontend/src/components/Bouteille/Bouteille.jsx
+++ frontend/src/components/Bouteille/Bouteille.jsx
@@ -1,41 +1,43 @@
 import "./Bouteille.scss";
+import bottles from "../../assets/images/wines.jpg";
 
 function Bouteille() {
   return (
-    <div className="">
-      <div className="container_bouteilles">
-        <div className="element_bouteille">
-          <div className="element_photo_gauche">
-            <image src="" />
-          </div>
-          <div className="element_text_droite">
-            <h2>PADDLE</h2>
-            <p>POOP</p>
-          </div>
+    <div className="container_bouteilles">
+      <div className="element_bouteille">
+        <div className="element_photo_gauche">
+          <image src={bottles} className="element_photo" />
         </div>
-
-        <div className="element_bouteille">
-          <div className="element_photo_gauche">
-            <image src="" />
-          </div>
-          <div className="element_text_droite">
-            <h2>FLADDLE</h2>
-            <p>POOP</p>
-          </div>
+        <div className="element_text_droite">
+          <h2>PADDLE</h2>
+          <p>POOP</p>
         </div>
-
-        <div className="element_bouteille">
-          <div className="element_photo_gauche">
-            <image src="" />
-          </div>
-          <div className="element_text_droite">
-            <h2>SHIZZLE</h2>
-            <p>POOP</p>
-          </div>
-        </div>
-
-        <div className="element_bouton" />
       </div>
+
+      <div className="element_bouteille">
+        <div className="element_photo_gauche">
+          <image src={bottles} className="element_photo" />
+        </div>
+        <div className="element_text_droite">
+          <h2>FLADDLE</h2>
+          <p>POOP</p>
+        </div>
+      </div>
+
+      <div className="element_bouteille">
+        <div className="element_photo_gauche">
+          <image src={bottles} className="element_photo" />
+        </div>
+        <div className="element_text_droite">
+          <h2>SHIZZLE</h2>
+          <p>POOP</p>
+        </div>
+      </div>
+
+      <button type="button" className="home_btn">
+        {" "}
+        Bravo ! Un avis ?
+      </button>
     </div>
   );
 }
