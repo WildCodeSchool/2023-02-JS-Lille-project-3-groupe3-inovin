32e456346f2dec118dde44da979fb33e64f36f4f
mlAgostini <valmont.vakarian@gmail.com>
2023-06-21 10:16:26 +0200

    Commit before merge of dev

--- frontend/src/components/SliderFiche/SliderFiche.jsx
+++ frontend/src/components/SliderFiche/SliderFiche.jsx
@@ -1,5 +1,25 @@
+// import { useState } from "react";
+
+import "./SliderFiche.scss";
+
 function SliderFiche() {
-  return <div />;
+  // const [activeTab, setActiveTab] = useState("tab1");
+
+  return (
+    <div>
+      {/* <h3>Vin numéro 1</h3>
+      <div className="tabs">
+        Tab nav
+        <ul className="nav">
+          <li className="tab1">Onglet Robe</li>
+          <li className="tab2">Onglet Arôme</li>
+          <li className="tab3">Onglet Saveur</li>
+        </ul>
+        Content of the tabs
+        <div className="outlet"></div>
+      </div> */}
+    </div>
+  );
 }
 
 export default SliderFiche;
--- frontend/src/pages/FicheDegustation/FicheDegustation.jsx
+++ frontend/src/pages/FicheDegustation/FicheDegustation.jsx
@@ -1,5 +1,12 @@
+import SliderFiche from "../../components/SliderFiche/SliderFiche";
+
 function FicheDegustation() {
-  return <div />;
+  return (
+    <div>
+      <h2>Fiche de dégustation</h2>
+      <SliderFiche />
+    </div>
+  );
 }
 
 export default FicheDegustation;
