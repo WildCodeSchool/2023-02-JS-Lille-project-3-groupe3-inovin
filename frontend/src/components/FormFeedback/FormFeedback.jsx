840a7dcb83458ccac0fb8eb22f17d5d89fed0421
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-06-21 14:45:54 +0200

    add function & using back

--- frontend/src/components/FormFeedback/FormFeedback.jsx
+++ frontend/src/components/FormFeedback/FormFeedback.jsx
@@ -2,13 +2,17 @@ import { useState, useEffect } from "react";
 import "./FormFeedback.scss";
 import { FaStar } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import separator from "../../assets/images/separator02.png";
 import fleche from "../../assets/images/fleche_360.png";
 
 function FormFeedback() {
   const [rating, setRating] = useState(null);
   const [hover, setHover] = useState(null);
+  const [userComment, setUserComment] = useState("");
   const [isPortrait, setIsPortrait] = useState(window.innerWidth < 769);
+
+  const [feedbackConfirm, setFeedbackConfirm] = useState(false);
 
   const updateMedia = () => {
     setIsPortrait(window.innerWidth < 769);
@@ -25,6 +29,33 @@ function FormFeedback() {
     navigate("/Resume");
   };
 
+  const sendFeedbackRating = () => {
+    const userId = 3; // id de l'utilisateur connecté
+
+    const user = {
+      // get les données de l'utilisateur connecté
+      firstname: "khouloud",
+      lastname: "belkhir",
+      birthdate: "1992-12-26",
+      address: "koukabelle@gmail.com",
+      ordering: 0,
+      feedbackRating: rating,
+      feedbackComment: userComment,
+      user_type: "utilisateur",
+    };
+
+    axios
+      .put(`${import.meta.env.VITE_BACKEND_URL}/user/${userId}`, user)
+      .then((response) => {
+        setFeedbackConfirm(true);
+        response.send("Feedback envoyé avec succès !");
+      })
+      .catch((error) => {
+        // Gérez les erreurs de la requête ici
+        console.error("Erreur lors de l'envoi du feedback :", error);
+      });
+  };
+
   return (
     <div className="commentsPage">
       <div className="commentsBook">
@@ -33,7 +64,11 @@ function FormFeedback() {
       <img src={separator} className="separatorImg" alt="artdeco" />
 
       <div className="noticeContainer">
-        <h3 className="noticeTitle">AVIS</h3>
+        {!feedbackConfirm ? (
+          <h3 className="noticeTitle">AVIS</h3>
+        ) : (
+          <h3 className="feedbackSent">Ton avis a bien été envoyé, merci !</h3>
+        )}
 
         <div className="content">
           <h3 className="rateExpTitle">NOTE L'EXPERIENCE DE L'ATELIER :</h3>
@@ -64,14 +99,32 @@ function FormFeedback() {
           <h3 className="askCommentTitle">UN PETIT COMMENTAIRE ?</h3>
 
           {isPortrait ? (
-            <textarea className="commentsArea" rows="7" cols="33" />
+            <textarea
+              className="commentsArea"
+              rows="7"
+              cols="33"
+              placeholder="Votre commentaire..."
+              value={userComment}
+              onChange={(event) => setUserComment(event.target.value)}
+            />
           ) : (
-            <textarea className="commentsArea" rows="6" cols="60" />
+            <textarea
+              className="commentsArea"
+              rows="6"
+              cols="60"
+              placeholder="Votre commentaire..."
+              value={userComment}
+              onChange={(event) => setUserComment(event.target.value)}
+            />
           )}
         </div>
 
         <div className="buttonDecoration">
-          <button type="submit" className="sendForm">
+          <button
+            type="submit"
+            className="sendForm"
+            onClick={sendFeedbackRating}
+          >
             ENVOYER
           </button>
         </div>
