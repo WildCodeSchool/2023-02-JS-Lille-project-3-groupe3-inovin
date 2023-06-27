b5afe182332cf00e07cb13ebf11e756b64be6135
MarieVanGreves <keiiskyx@gmail.com>
2023-06-27 16:39:29 +0200

    Second admin page CSS done. Front and back need to be connected.

--- frontend/src/pages/AdminAjoutVin/AdminAjoutVin.jsx
+++ frontend/src/pages/AdminAjoutVin/AdminAjoutVin.jsx
@@ -12,21 +12,21 @@ function AdminAjoutVin() {
         </label>
 
         <label className="titleMiniInfo">
-          Nom du vin:
+          Région:
           <input type="text" name="name" className="inputAjoutLabel" />
         </label>
         <label className="titleMiniInfo">
-          Nom du vin:
-          <input type="text" name="name" className="inputAjoutLabel" />
-        </label>
-
-        <label className="titleMiniInfo">
-          Nom du vin:
+          Couleur:
           <input type="text" name="name" className="inputAjoutLabel" />
         </label>
 
         <label className="titleMiniInfo">
-          Nom du vin:
+          Année:
+          <input type="text" name="name" className="inputAjoutLabel" />
+        </label>
+
+        <label className="titleMiniInfo">
+          Cépage:
           <input type="text" name="name" className="inputAjoutLabel" />
         </label>
         <input
@@ -35,7 +35,32 @@ function AdminAjoutVin() {
           className="boutonSubmitAdmin2"
         />
       </form>
+
       <div className="containerTraitNoir" />
+
+      <div className="containerRechercheAjoutVin">
+        <h3 className="rechercheAjoutVin">RECHERCHER :</h3>
+        <input
+          type="search"
+          placeholder="R. par nom de vin"
+          className="inputRechercheVin"
+        />
+      </div>
+      <div className="containerInfoRechercheVin">
+        <h3 className="titleMiniInfo">Nom de la bouteille de vin :</h3>
+        <h3 className="titleMiniInfo">Région :</h3>
+        <h3 className="titleMiniInfo">Couleur :</h3>
+        <h3 className="titleMiniInfo">Année :</h3>
+        <h3 className="titleMiniInfo">Cépage :</h3>
+      </div>
+      <div className="containerBoutonInfoVin">
+        <button className="deuxBoutons" type="submit" value="MODIFIER LE VIN">
+          MODIFIER LE VIN
+        </button>
+        <button className="deuxBoutons" type="submit" value="MODIFIER LE VIN">
+          SUPPRIMER LE VIN
+        </button>
+      </div>
     </div>
   );
 }
