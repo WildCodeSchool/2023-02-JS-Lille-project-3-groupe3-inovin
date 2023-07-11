bc256dca339a2f7f6d926df92a230b4f84310f4f
ANNE MARCHASSON <annemarchasson@yahoo.fr>
2023-07-11 16:09:42 +0200

    test get id on animation bottle

--- frontend/src/components/BouteilleAnimation/AnimationBottle.jsx
+++ frontend/src/components/BouteilleAnimation/AnimationBottle.jsx
@@ -1,13 +1,28 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
 import axios from "axios";
 import "./AnimationBottle.scss";
+import UserContext from "../../contexts/UserContext";
 
 function AnimationBottle() {
   const [progress, setProgress] = useState(0); // État pour gérer la progression
   const [fixedProgress, setFixedProgress] = useState(null); // État pour stocker la progression fixée
   const [isLocked, setIsLocked] = useState(false); // État pour indiquer si la progression est verrouillée
+  const [wineBottleId] = useState(null); // État pour stocker l'ID de la bouteille de vin
   const progressBarRef = useRef(null); // Référence à l'élément de la barre de progression pour ne pas scroller sur body entier
-
+  const { user } = useContext(UserContext); // account_id of current user from inscription page, you can use it for update database
+  // Effectuer la requête GET pour obtenir wineBottle_id depuis la table compo_recipe
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/user`, {
+        params: { account_id: `${user}` },
+      })
+      .then((response) => {
+        response.Send(200);
+      })
+      .catch((error) => {
+        console.error("Error retrieving composition recipe details:", error);
+      });
+  }, []);
   useEffect(() => {
     // Écouteur d'événement pour la souris
     const handleMouseMove = (e) => {
@@ -15,13 +30,9 @@ function AnimationBottle() {
       const progressBarHeight = progressBar.offsetHeight;
       const { top } = progressBar.getBoundingClientRect();
       const maxProgress = 100;
-
       // progressBar : référence de la barre de progression à partir de progressBarRef.current. La référence est stockée dans la variable progressBar
-
       // progressBarHeight : hauteur de la barre de progression à l'aide de progressBar.offsetHeight. La hauteur est stockée dans la variable progressBarHeight
-
       // { top } : Cela déstructure les propriétés top de la valeur retournée par progressBar.getBoundingClientRect(). La méthode getBoundingClientRect() renvoie un objet contenant des informations sur les dimensions et la position d'un élément par rapport à la fenêtre. déstructuration, extraire valeur de top et socker dans variable top.
-
       if (!isLocked && fixedProgress === null) {
         // Récupération de la position verticale de la souris par rapport à la barre de progression
         const mouseY = e.clientY - top;
@@ -33,58 +44,41 @@ function AnimationBottle() {
           ),
           maxProgress
         );
-
         setProgress(newProgress); // Mettre à jour l'état avec la nouvelle progression
       }
     };
-
     document.addEventListener("mousemove", handleMouseMove);
-
     return () => {
       document.removeEventListener("mousemove", handleMouseMove);
     };
   }, [isLocked, fixedProgress]);
-
-  // ...
-  const progressRef = useRef(null); // Référence pour stocker la valeur de progress lors du clic
-
-  // ...
-
   const handleClick = () => {
     if (fixedProgress === null) {
-      progressRef.current = progress; // Stocker la valeur actuelle de progress
-
       setFixedProgress(progress); // Fixer la progression
       setIsLocked(true); // Verrouiller la progression
-
       // Effectuer la requête POST pour envoyer la valeur progress et wineBottleId
       axios
         .post(`${import.meta.env.VITE_BACKEND_URL}/compo_recipe`, {
-          percentage: progressRef.current, // Utiliser la valeur stockée de progress
-          user_id: 3,
-          wineBottle_id: 1,
-          user_account_ID: 1,
+          percentage: progress,
+          wineBottle_id: wineBottleId,
         })
         .then()
         .catch((error) => {
           console.error(
-            "Une erreur s'est produite lors de l'envoi des données :",
+            "Une erreur s'est produite lors de l'envoi de la valeur progress et wineBottleId!",
             error
           );
         });
     } else {
-      setIsLocked(false);
+      setIsLocked(false); // Déverrouiller la progression
     }
   };
-  // ...
-
   // obligation pour passer le commit (enter sur clavier)
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
       handleClick();
     }
   };
-
   return (
     <div className="bottle-animation">
       <div
@@ -110,5 +104,4 @@ function AnimationBottle() {
     </div>
   );
 }
-
 export default AnimationBottle;
