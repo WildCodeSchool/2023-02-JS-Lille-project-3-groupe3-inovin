d6a6746d6c69e21fe567f4fcdea13d3f97e6f930
ANNE MARCHASSON <annemarchasson@yahoo.fr>
2023-07-15 16:32:54 +0200

    update button validate in slide flavor

--- frontend/src/components/SliderFiche/FormVin1.jsx
+++ frontend/src/components/SliderFiche/FormVin1.jsx
@@ -114,24 +114,6 @@ function FormVin1({ firstBottleId }) {
   return (
     <div className="contenair-carousel">
       <h3 className="h3-title-wine-taste">VIN NUMERO 1</h3>
-
-      {isEditing ? (
-        <button
-          className="btn-update-form"
-          type="button"
-          onClick={handleEditForm1}
-        >
-          Modifier
-        </button>
-      ) : (
-        <button
-          className="btn-validate-form"
-          type="button"
-          onClick={handleSubmitForm1}
-        >
-          Valider
-        </button>
-      )}
       <div className="slideWrapper">
         <div className="thirdSlide">
           <div className="tabs">
@@ -478,6 +460,23 @@ function FormVin1({ firstBottleId }) {
                     })}
                   </div>
                 </form>
+                {isEditing ? (
+                  <button
+                    className="btn-update-form"
+                    type="button"
+                    onClick={handleEditForm1}
+                  >
+                    Modifier
+                  </button>
+                ) : (
+                  <button
+                    className="btn-validate-form"
+                    type="button"
+                    onClick={handleSubmitForm1}
+                  >
+                    Valider
+                  </button>
+                )}
               </TabContent>
             </div>
           </div>
